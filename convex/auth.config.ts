d004c20211f89cfde0f3a1a1ea09840bdbd47487
Aaron <xiaofengliu1992@hotmail.com>
2024-05-13 22:29:40 -0400

    init set up

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+        {
+            domain: "https://funny-tuna-1.clerk.accounts.dev",
+            applicationID: 'convex'
+        },
+    ],
+};
--- src/app/ConvexClientProvider.tsx
+++ src/app/ConvexClientProvider.tsx
@@ -1,11 +1,23 @@
-"use client"
+"use client";
 import { ReactNode } from "react";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
 
 export default function ConvexClientProvider({
-    children,
-} : {children : ReactNode}) {
-    return <ConvexProvider client={convex}>{children}</ConvexProvider>
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
 }
